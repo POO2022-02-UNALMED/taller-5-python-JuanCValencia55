499ba20c9918d297caa2f20e4cd3cca3fc405f2d
GiBsy <jcval2801@gmail.com>
2022-09-21 13:39:57 -0500

    try2

---
+++ zooAnimales/Ave.py
@@ -0,0 +1,39 @@
+from animal import Animal
+
+class Ave(Animal):
+    _listado = []
+    halcones = 0
+    aguilas = 0
+
+    def __init__(self, nombre = "", edad = 0, habitat = "", genero = "", colorPlumas = ""):
+        super().__init__(nombre, edad, habitat, genero)
+        self._colorPlumas = colorPlumas
+        Ave._listado.append(self)
+
+    #metodos get
+    def getColorPlumas(self):
+        return self._colorPlumas
+
+    #metodos set
+    def setColorPlumas(self, color):
+        self._colorPlumas = color
+    
+    #metodos de la clase
+    def movimiento(self):
+        return "volar"
+    
+    @classmethod
+    def cantidadAves(cls):
+        return len(cls._listado)
+    
+    @classmethod
+    def crearHalcon(cls, nombre = "", edad = 0, genero = ""):
+        halcon = Ave(nombre, edad, "montanas", genero, "cafe glorioso")
+        cls.halcones += 1
+        return halcon
+    
+    @classmethod
+    def crearAguila(cls, nombre = "", edad = 0, genero = ""):
+        aguila = Ave(nombre, edad, "montanas", genero, "blanco y amarillo")
+        cls.aguilas += 1
+        return aguila
---
+++ zooAnimales/Pez.py
@@ -0,0 +1,47 @@
+from codecs import backslashreplace_errors
+from animal import Animal
+
+class Pez(Animal):
+    _listado = []
+    salmones = 0
+    bacalaos = 0
+
+    def __init__(self, nombre = "", edad = 0, habitat = "", genero = "", colorEscamas = "", cantidadAletas = ""):
+        super().__init__(nombre, edad, habitat, genero)
+        self._colorEscamas = colorEscamas
+        self._cantidadAletas = cantidadAletas
+        Pez._listado.append(self)
+    
+    #metodos get
+    def getColorEscamas(self):
+        return self._colorEscamas
+    
+    def getCantidadAletas(self):
+        return self._cantidadAletas
+    
+    #metodos set
+    def setColorEscamas(self, color):
+        self._colorEscamas = color
+    
+    def setCantidadAletas(self, cantidad):
+        self._cantidadAletas = cantidad
+    
+    #metodos de la clase
+    def movimiento(self):
+        return "nadar"
+    
+    @classmethod
+    def cantidadPeces(cls):
+        return len(cls._listado)
+    
+    @classmethod
+    def crarSalmon(cls, nombre = "", edad = 0, genero = ""):
+        salmon = Pez(nombre, edad, "oceano", genero, "rojo", 6)
+        cls.salmones +=1
+        return salmon
+
+    @classmethod
+    def crarBacalao(cls, nombre = "", edad = 0, genero = ""):
+        bacalao = Pez(nombre, edad, "oceano", genero, "gris", 6)
+        cls.salmones +=1
+        return bacalao
